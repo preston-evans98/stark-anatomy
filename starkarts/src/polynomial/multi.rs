46e843fea4b86282949623e608f2d0f20d230a19
Preston Evans <pbevans1@crimson.ua.edu>
2022-02-01 22:10:59 -0600

    Remove one layer of generics from Multivariate poly methods

--- starkarts/src/polynomial/multi.rs
+++ starkarts/src/polynomial/multi.rs
@@ -1,37 +1,21 @@
 use std::{collections::HashMap, hash::Hash};
 
-use crate::{field_elem::FieldElement, P};
-type exponent = usize;
+use crate::field_elem::FieldElement;
 
 #[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
-pub struct Exponents<const VARIABLES: usize>([exponent; VARIABLES]);
+pub struct Exponents<const VARIABLES: usize>([usize; VARIABLES]);
 
 impl<const VARIABLES: usize> Exponents<VARIABLES> {
     const fn new() -> Exponents<VARIABLES> {
         Self([0usize; VARIABLES])
     }
-    fn pad<const NEW_LEN: usize>(&self) -> Exponents<NEW_LEN> {
-        let mut result = [0; NEW_LEN];
-        result[..VARIABLES].copy_from_slice(&self.0);
-        Exponents(result)
-    }
-    const fn get_size() -> usize {
-        VARIABLES
-    }
 }
 
-impl<const VARIABLES: usize, const OTHER: usize> std::ops::Mul<Exponents<OTHER>>
-    for Exponents<VARIABLES>
-where
-    [(); max(VARIABLES, OTHER)]:,
-{
-    type Output = Exponents<{ max(VARIABLES, OTHER) }>;
+impl<const VARIABLES: usize> std::ops::Mul for Exponents<VARIABLES> {
+    type Output = Self;
 
-    fn mul(self, rhs: Exponents<OTHER>) -> Self::Output {
-        let mut result = <Self as std::ops::Mul<Exponents<OTHER>>>::Output::new();
-        for (res, l) in result.0.iter_mut().zip(self.0.iter()) {
-            *res += *l
-        }
+    fn mul(self, rhs: Self) -> Self::Output {
+        let mut result = self;
         for (res, r) in result.0.iter_mut().zip(rhs.0.iter()) {
             *res += *r
         }
@@ -46,17 +30,24 @@ pub struct MultiVariatePoly<F, const VARIABLES: usize> {
     dict: HashMap<Exponents<VARIABLES>, F>,
 }
 
-impl<F: FieldElement, const VARIABLES: usize> MultiVariatePoly<F, VARIABLES>
-where
-    [(); { max(VARIABLES, VARIABLES) }]:,
-{
+impl<F: FieldElement, const VARIABLES: usize> MultiVariatePoly<F, VARIABLES> {
     fn zero_exponent() -> Exponents<VARIABLES> {
         Exponents::<VARIABLES>::new()
     }
-    pub fn pow(self, mut exponent: usize) -> Self {
+    pub fn zero() -> Self {
+        Self {
+            dict: HashMap::new(),
+        }
+    }
+    pub fn with_capacity(c: usize) -> Self {
+        Self {
+            dict: HashMap::with_capacity(c),
+        }
+    }
+    pub fn pow(&self, mut exponent: usize) -> Self {
         let dict = HashMap::from([(Self::zero_exponent(), F::one())]);
         let mut acc = MultiVariatePoly { dict };
-        let mut base = self;
+        let mut base = self.clone();
         while exponent != 0 {
             if exponent & 1 == 0 {
                 base = &base * &base;
@@ -70,128 +61,53 @@ where
     }
 }
 
-impl<F, const VARIABLES: usize> MultiVariatePoly<F, VARIABLES> {
-    fn insert_padded<const OTHER: usize>(&mut self, exponents: Exponents<OTHER>, value: F) {
-        let exponents = exponents.pad::<VARIABLES>();
+impl<F: FieldElement, const VARIABLES: usize> MultiVariatePoly<F, VARIABLES> {
+    fn insert(&mut self, exponents: Exponents<VARIABLES>, value: F) {
         self.dict.insert(exponents, value);
     }
 
-    unsafe fn unsafe_insert<const OTHER: usize>(
-        &mut self,
-        mut exponents: Exponents<OTHER>,
-        value: F,
-    ) -> Option<F> {
-        // Hack for tranmuting const generic arrays taken from: https://github.com/rust-lang/rust/issues/61956
-        let ptr = &mut exponents.0 as *mut _ as *mut [exponent; VARIABLES];
-        let res = ptr.read();
-        self.dict.insert(Exponents(res), value)
-    }
-
-    // fn insert_le(&mut self, exponents: Exponents<VARIABLES>, value: F) {
-    //     if VARIABLES < OTHER {
-    //         let padded = exponents.pad::<OTHER>();
-    //     }
-    //     self.dict.insert(exponents, value);
-    // }
-}
-
-impl<F, const VARIABLES: usize> MultiVariatePoly<F, VARIABLES> {
-    pub fn zero() -> Self {
-        Self {
-            dict: HashMap::new(),
+    /// Add a single term to the polynomial. For example (3xy + 4x).add_term(2x) = (3xy + 6x)
+    pub fn add_term(&mut self, exponents: Exponents<VARIABLES>, coef: F) {
+        match self.dict.insert(exponents, coef) {
+            Some(existing_coef) => {
+                self.dict.insert(exponents, coef * existing_coef);
+            }
+            None => {}
         }
     }
 }
 
-pub const fn max(lhs: usize, rhs: usize) -> usize {
-    if lhs > rhs {
-        lhs
-    } else {
-        rhs
-    }
-}
-
-impl<F, const VARIABLES: usize, const OTHER: usize> std::ops::Add<&MultiVariatePoly<F, OTHER>>
-    for &MultiVariatePoly<F, VARIABLES>
+impl<F, const VARIABLES: usize> std::ops::Add for &MultiVariatePoly<F, VARIABLES>
 where
-    [(); max(VARIABLES, OTHER)]:,
     F: FieldElement,
 {
-    type Output = MultiVariatePoly<F, { max(VARIABLES, OTHER) }>;
+    type Output = MultiVariatePoly<F, VARIABLES>;
 
-    fn add(self, rhs: &MultiVariatePoly<F, OTHER>) -> Self::Output {
-        let mut result = Self::Output::zero();
-        if VARIABLES > OTHER {
-            for (exponents, coefficient) in rhs.dict.iter() {
-                result.insert_padded(*exponents, *coefficient);
-            }
-            for (exponents, coefficient) in self.dict.iter() {
-                unsafe {
-                    match result.unsafe_insert(*exponents, *coefficient) {
-                        Some(existing) => {
-                            result.unsafe_insert(*exponents, *coefficient + existing);
-                        }
-                        None => {}
-                    }
-                }
-            }
-            return result;
-        } else if VARIABLES == OTHER {
-            for (exponents, coefficient) in rhs.dict.iter() {
-                unsafe {
-                    result.unsafe_insert(*exponents, *coefficient);
-                }
-            }
-            for (exponents, coefficient) in self.dict.iter() {
-                unsafe {
-                    match result.unsafe_insert(*exponents, *coefficient) {
-                        Some(existing) => {
-                            result.unsafe_insert(*exponents, *coefficient + existing);
-                        }
-                        None => {}
-                    }
-                }
-            }
-        } else {
-            for (exponents, coefficient) in self.dict.iter() {
-                result.insert_padded(*exponents, *coefficient)
-            }
-            for (exponents, coefficient) in rhs.dict.iter() {
-                unsafe {
-                    match result.unsafe_insert(*exponents, *coefficient) {
-                        Some(existing) => {
-                            result.unsafe_insert(*exponents, *coefficient + existing);
-                        }
-                        None => {}
-                    }
-                }
-            }
+    fn add(self, rhs: Self) -> Self::Output {
+        let mut result = MultiVariatePoly::with_capacity(self.dict.len() + rhs.dict.len());
+        for (exp, coef) in self.dict.iter() {
+            result.insert(*exp, *coef);
         }
-
+        for (exp, coef) in rhs.dict.iter() {
+            result.add_term(*exp, *coef);
+        }
         result
     }
 }
 
-impl<F, const VARIABLES: usize, const OTHER: usize> std::ops::Mul<&MultiVariatePoly<F, OTHER>>
-    for &MultiVariatePoly<F, VARIABLES>
+impl<F, const VARIABLES: usize> std::ops::Mul for &MultiVariatePoly<F, VARIABLES>
 where
-    [(); max(VARIABLES, OTHER)]:,
     F: FieldElement,
 {
-    type Output = MultiVariatePoly<F, { max(VARIABLES, OTHER) }>;
+    type Output = MultiVariatePoly<F, VARIABLES>;
 
-    fn mul(self, rhs: &MultiVariatePoly<F, OTHER>) -> Self::Output {
-        let mut result = MultiVariatePoly::zero();
+    fn mul(self, rhs: Self) -> Self::Output {
+        let mut result = MultiVariatePoly::with_capacity(self.dict.len() * rhs.dict.len());
         for (exp1, coef1) in self.dict.iter() {
             for (exp2, coef2) in rhs.dict.iter() {
                 let coef = *coef1 * *coef2;
                 let exp = *exp1 * *exp2;
-                match result.dict.insert(exp, coef) {
-                    Some(existing_coef) => {
-                        result.dict.insert(exp, existing_coef + coef);
-                    }
-                    None => {}
-                }
+                result.add_term(exp, coef)
             }
         }
         result
@@ -210,15 +126,20 @@ impl<F: FieldElement, const VARIABLES: usize> std::ops::Neg for &MultiVariatePol
     }
 }
 
-impl<F, const VARIABLES: usize, const OTHER: usize> std::ops::Sub<&MultiVariatePoly<F, OTHER>>
-    for &MultiVariatePoly<F, VARIABLES>
+impl<F, const VARIABLES: usize> std::ops::Sub for &MultiVariatePoly<F, VARIABLES>
 where
-    [(); max(VARIABLES, OTHER)]:,
     F: FieldElement,
 {
-    type Output = MultiVariatePoly<F, { max(VARIABLES, OTHER) }>;
+    type Output = MultiVariatePoly<F, VARIABLES>;
 
-    fn sub(self, rhs: &MultiVariatePoly<F, OTHER>) -> Self::Output {
-        self + &(-rhs)
+    fn sub(self, rhs: Self) -> Self::Output {
+        let mut result = MultiVariatePoly::with_capacity(self.dict.len() + rhs.dict.len());
+        for (exp, coef) in self.dict.iter() {
+            result.insert(*exp, -*coef);
+        }
+        for (exp, coef) in rhs.dict.iter() {
+            result.add_term(*exp, -*coef)
+        }
+        result
     }
 }
